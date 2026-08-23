68c85eee65581c960c8987dcf1cdfed794a152e9
zubairahmad <zubairahm72@gmail.com>
2026-08-23 12:24:11 +0530

    fix() student and teacher login

--- app/main.py
+++ app/main.py
@@ -39,4 +39,6 @@ async def health_check():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app.main:app", host="0.0.0.0", port=5001, reload=True)
+    import os
+    port = int(os.environ.get("PORT", 5001))
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)
